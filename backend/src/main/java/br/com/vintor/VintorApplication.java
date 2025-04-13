571b963b14933236b2cca7398f7c97edd25c3f14
Jean da Rocha Vertuoso <jean_b123@hotmail.com>
2025-04-13 00:38:44 -0300

    estrutura do backend criada

---
+++ backend/src/main/java/br/com/vintor/VintorApplication.java
@@ -0,0 +1,13 @@
+package br.com.vintor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/vintor/VintorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vintor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VintorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
